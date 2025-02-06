1f2130acfb26bad21dc4cf2c907278128a145fc0
b7cx <oxycodeineinc@gmail.com>
2025-02-05 20:45:56 -0600

    Update obsidianloader.lua

--- obsidianloader.lua
+++ obsidianloader.lua
@@ -3,6 +3,6 @@ if not game:IsLoaded() then
 end
 
 if getgenv().Obsidian then
-    script_key = getgenv().Script.Core.Key
+    script_key = getgenv().Obsidian.Core.Key
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2ef0e945807310ad46a3446b6f7fbca5.lua"))()
 end
